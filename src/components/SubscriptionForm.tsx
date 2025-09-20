c5208a77331a46842562e1f656fe20bc9eb5c6d3
조예진 <orobos654@gmail.com>
2025-09-20 16:08:47 +0900

    feat: fix form contents

--- src/components/SubscriptionForm.tsx
+++ src/components/SubscriptionForm.tsx
@@ -1,6 +1,11 @@
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { subscriptionFormSchema, type SubscriptionFormValue } from '../schema/subscriptionFormSchema';
+import {
+  subscriptionFormSchema,
+  type SubscriptionFormValue,
+  subscriptionFormYears,
+  SubscriptionYearToNameMap
+} from '../schema/subscriptionFormSchema';
 
 export const SubscriptionForm = ({ onSubmit }: {
   onSubmit: (data: SubscriptionFormValue) => void
@@ -15,8 +20,10 @@ export const SubscriptionForm = ({ onSubmit }: {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      <h2>신청 폼</h2>
+      <p>이메일과 FE 경력 연차 등 간단한 정보를 입력해주세요.</p>
       <label>
-        이름
+        이름 / 닉네임
         <input type="text" placeholder={'이름을 입력해주세요'} {...register('name')}/>
       </label>
       {errors.name && <p>{errors.name.message}</p>}
@@ -25,6 +32,21 @@ export const SubscriptionForm = ({ onSubmit }: {
         <input type="email" inputMode={'email'} placeholder={'이메일을 입력해주세요'} {...register('email')}/>
       </label>
       {errors.email && <p>{errors.email.message}</p>}
+      <label>
+        FE 경력 연차
+        <select {...register('years')}>
+          {subscriptionFormYears.options.map(value => (
+            <option value={value}>{SubscriptionYearToNameMap[value]}</option>
+          ))}
+        </select>
+      </label>
+      {errors.years && <p>{errors.years.message}</p>}
+      <label>
+        GitHub 링크 (선택)
+        <input type="url" inputMode={'url'} placeholder={'https://github.com/username'} {...register('github')}/>
+      </label>
+      {errors.github && <p>{errors.github.message}</p>}
+
       <button type={'submit'}>신청</button>
     </form>
   )
--- src/schema/subscriptionFormSchema.ts
+++ src/schema/subscriptionFormSchema.ts
@@ -1,8 +1,18 @@
 import { z } from 'zod';
 
+export const subscriptionFormYears = z.enum([ '0-3', '4-7', '8+' ], { error: '연차를 선택해주세요' });
+
+export const SubscriptionYearToNameMap: Record<(typeof subscriptionFormYears['options'])[number], string> = {
+  '0-3': '0-3년',
+  '4-7': '4-7년',
+  '8+': '8년 이상'
+}
+
 export const subscriptionFormSchema = z.object({
   name: z.string().min(1, { message: '이름을 입력해주세요' }),
   email: z.email({ error: '이메일을 정확히 입력해주세요' }),
+  years: subscriptionFormYears,
+  github: z.url({ hostname: /^github\.com$/, error: 'github url을 입력해주세요' }).optional()
 })
 
 export type SubscriptionFormValue = z.infer<typeof subscriptionFormSchema>
