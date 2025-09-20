8fc13ea04e7313be1340e0dbec60ec9483c2ea49
조예진 <orobos654@gmail.com>
2025-09-20 15:36:12 +0900

    refactor: add ref props to triggerElement

--- src/ModalFormPage.tsx
+++ src/ModalFormPage.tsx
@@ -11,15 +11,18 @@ const ModalFormPage = () => {
     <FormContextProvider<SubscriptionFormValue>
       formElement={
         ({onSubmit}) => <>
-          {/* TODO: children을 밖에서 받아올 수 있게 */}
           <h2>뭔가 신청하기</h2>
           <div style={{height: '150vh'}}>아주 긴 무언가</div>
           <SubscriptionForm onSubmit={onSubmit}/>
         </>
       }
       triggerElement={
-        ({open}) => (
-          <button onClick={() => handleOpenForm(open)} style={{marginTop: '100vh'}}>
+        ({open, ref}) => (
+          <button
+            ref={ref}
+            onClick={() => handleOpenForm(open)}
+            style={{marginTop: '100vh'}}
+          >
             신청하기
           </button>
         )
--- src/context/FormProvider.tsx
+++ src/context/FormProvider.tsx
@@ -1,9 +1,9 @@
-import {type ReactNode, useCallback, useRef, useState} from "react";
+import {type ReactNode, type Ref, useCallback, useRef, useState} from "react";
 import {Modal} from "../components/Modal";
 
 type FormProviderProps<FormValue> = {
   formElement: (props: { onSubmit: (data: FormValue) => void }) => ReactNode,
-  triggerElement: (props: { open: () => Promise<FormValue | null> }) => ReactNode
+  triggerElement: (props: { open: () => Promise<FormValue | null>, ref: Ref<HTMLButtonElement | null> }) => ReactNode
 }
 
 export const FormContextProvider = <FormValue extends object>
@@ -14,16 +14,15 @@ export const FormContextProvider = <FormValue extends object>
   const [open, setOpen] = useState(false)
 
   const resolveModalResponse = useRef<(value: FormValue | null) => void | null>(null)
-  const previousActiveElementRef = useRef<HTMLElement>(null)
+  const triggerElementRef = useRef<HTMLButtonElement>(null)
 
   const handleClose = useCallback((value: FormValue | null) => {
     resolveModalResponse.current?.(value);
     setOpen(false);
-    previousActiveElementRef.current?.focus?.();
+    triggerElementRef.current?.focus();
   }, [])
 
   const handleOpen = useCallback((): Promise<FormValue | null> => {
-    previousActiveElementRef.current = document.activeElement as HTMLElement;
     setOpen(true);
     return new Promise(resolve =>
       resolveModalResponse.current = resolve
@@ -31,7 +30,7 @@ export const FormContextProvider = <FormValue extends object>
   }, [])
 
   return <>
-    {triggerElement({open: handleOpen})}
+    {triggerElement({open: handleOpen, ref: triggerElementRef})}
     {open && <Modal onClose={handleClose}>
       {formElement({onSubmit: handleClose})}
     </Modal>}
